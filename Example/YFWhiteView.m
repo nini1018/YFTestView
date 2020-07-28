0f4abd5b7a0d13d245313ac1484dfb1920590e0d
fang yang <yangfang@xiangshang360.com>
2020-07-28 13:29:47 +0800

    1.1.0

--- Example/YFWhiteView.m
+++ Example/YFWhiteView.m
@@ -10,12 +10,13 @@
 
 @implementation YFWhiteView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    
+    return self;
 }
-*/
 
 @end
