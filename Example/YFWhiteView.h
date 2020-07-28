eada3f5222fa7087315440f001e8ca59d8438e22
fang yang <yangfang@xiangshang360.com>
2020-07-28 13:11:41 +0800

    first version test

---
+++ Example/YFWhiteView.h
@@ -0,0 +1,17 @@
+//
+//  YFWhiteView.h
+//  YFTestView_Tests
+//
+//  Created by fang yang on 2020/7/28.
+//  Copyright © 2020 nini1018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YFWhiteView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/YFWhiteView.m
@@ -0,0 +1,21 @@
+//
+//  YFWhiteView.m
+//  YFTestView_Tests
+//
+//  Created by fang yang on 2020/7/28.
+//  Copyright © 2020 nini1018. All rights reserved.
+//
+
+#import "YFWhiteView.h"
+
+@implementation YFWhiteView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
